fefdba46b8db5257e1ce41c4751aae36b3185dd7
smsamiulhasansaim <hello.smsamiulhasan@gmail.com>
2026-03-16 07:05:04 +0600

    Lesson 1

---
+++ mian.dart
@@ -0,0 +1,10 @@
+void main() {
+  String name = "S M Samiul Hasan Saim ";
+  int age = 20;
+  double gpa = 3.75;
+  bool isStudent = true;
+  print("Name: $name");
+  print("Age: $age");
+  print("GPA: $gpa");
+  print("Is Student: $isStudent");
+}
