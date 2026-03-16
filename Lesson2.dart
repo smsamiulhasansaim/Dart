cf5d1287da4c5d0199137553b03ce2a67cdf44bb
smsamiulhasansaim <hello.smsamiulhasan@gmail.com>
2026-03-16 07:28:15 +0600

    Lesson2

---
+++ Lesson2.dart
@@ -0,0 +1,12 @@
+void main() {
+  int age = 25;
+  if (age >= 20) {
+    print("adult");
+  } else {
+    print("not adult");
+  }
+  List<String> skills = ["dart", "flutter", "python"];
+  for (var s in skills) {
+    print(s);
+  }
+}
