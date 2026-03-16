2bdf563aa7ebbc115782a998469ae7772eb4dea2
smsamiulhasansaim <hello.smsamiulhasan@gmail.com>
2026-03-16 08:17:58 +0600

    If-else

---
+++ If-Else.dart
@@ -0,0 +1,13 @@
+void main() {
+  int age = 14;
+
+  if (age >= 18) {
+    print("Tmi kI Adult!");
+  }
+  else if (age >=13){
+     print("Tmi teenager!");
+  }
+  else{
+    print("Tmi Soto!");
+  }
+}
